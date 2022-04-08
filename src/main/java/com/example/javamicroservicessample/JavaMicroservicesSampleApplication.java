37cb97b20f95e7586df68f63224dad90da8e2221
Keisy Daniel Barcellos <keisy@Laptop-Daniel.localdomain>
2022-04-08 16:51:17 -0300

    Started from Spring Boot

---
+++ src/main/java/com/example/javamicroservicessample/JavaMicroservicesSampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.javamicroservicessample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMicroservicesSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaMicroservicesSampleApplication.class, args);
+	}
+
+}
